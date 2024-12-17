4f3cd08d526a8ce868e38692079e6dc22bc0be0c
alanslays <nowickialan25@gmail.com>
2024-12-17 13:12:19 +0100

    Poprawienie wydajności i błędów

--- Zadanie13/Form1.cs
+++ Zadanie13/Form1.cs
@@ -2,14 +2,42 @@ namespace Zadanie13
 {
     public partial class Form1 : Form
     {
+        DinnerParty dinnerParty;
         public Form1()
         {
             InitializeComponent();
+            dinnerParty = new DinnerParty() { NumberOfPeople = 5 };
+            dinnerParty.CalculateCostOfDecorations(false);
+            dinnerParty.SetHealthyOption(true);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.SetHealthyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculateCost(healthyBox.Checked);
+            label3.Text = Cost.ToString("c");
         }
     }
 }
