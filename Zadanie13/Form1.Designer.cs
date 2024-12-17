36d6373ec42ce63d7f365a4ca28a550455df0c2a
alanslays <nowickialan25@gmail.com>
2024-12-17 12:03:32 +0100

    Dodano Zadanie13

---
+++ Zadanie13/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Zadanie13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            fancyBox = new CheckBox();
+            healthyBox = new CheckBox();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Ilość osób";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(12, 37);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(180, 31);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // fancyBox
+            // 
+            fancyBox.AutoSize = true;
+            fancyBox.Location = new Point(12, 74);
+            fancyBox.Name = "fancyBox";
+            fancyBox.Size = new Size(201, 29);
+            fancyBox.TabIndex = 2;
+            fancyBox.Text = "Dekoracje fantazyjne";
+            fancyBox.UseVisualStyleBackColor = true;
+            // 
+            // healthyBox
+            // 
+            healthyBox.AutoSize = true;
+            healthyBox.Location = new Point(12, 109);
+            healthyBox.Name = "healthyBox";
+            healthyBox.Size = new Size(147, 29);
+            healthyBox.TabIndex = 3;
+            healthyBox.Text = "Opcja zdrowa";
+            healthyBox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Koszt";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.Fixed3D;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label3.Location = new Point(122, 170);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 30);
+            label3.TabIndex = 5;
+            label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(322, 214);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(healthyBox);
+            Controls.Add(fancyBox);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Planista przyjęć";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private CheckBox fancyBox;
+        private CheckBox healthyBox;
+        private Label label2;
+        private Label label3;
+    }
+}
