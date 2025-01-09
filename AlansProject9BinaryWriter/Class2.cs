05427d46d14394fc61db8dd534495f4ed9072411
alanslays <nowickialan25@gmail.com>
2025-01-09 13:13:04 +0100

    Dodano kolejne zadanie

---
+++ AlansProject9BinaryWriter/Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using (StreamReader reader = new StreamReader("input.dat")) ;
+using (StreamWriter writer = new StreamWriter("output.dat", false))
+{
+    int position = 0;
+    while(!reader.EndOfStream)
+    {
+        char[] buffer = new char[16];
+        int charactersRead = reader.ReadBlock(buffer, 0, 16);
+        writer.Write("{0}: ", String.Format("{0:x4}", position));
+        position += charactersRead;
+        for (int i = 0; i < 16; i++)
+        {
+            if (i < charactersRead)
+            {
+                string hex = String.Format("{0:x2}", (byte)buffer[i]));
+                writer.Write(hex + " ");
+            }
+            else
+                writer.Write("  ");
+            if (int == 7)
+            {
+                writer.Write("--");
+            }
+            if (buffer[i] < 32 || buffer[i] > 250)
+            {
+            buffer[i] = ".";
+            }
+        }
+            string bufferContents = new string(buffer);
+            writer.WriteLine("  " + bufferContents.Substring(0, charactersRead));
+    }
+} 
