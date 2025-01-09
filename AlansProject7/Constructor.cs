608a50f09c776ff2b082f18411dd012a9f9ce401
alanslays <nowickialan25@gmail.com>
2025-01-09 10:55:30 +0100

    Dodano zadania

---
+++ AlansProject7/Constructor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+public class Worker
+{
+    public Worker(string[] jobsICanDo)
+    {
+        this.jobsICanDo;
+    }
+
+    public int ShiftsLeft
+    {
+        get
+        {
+            return shiftsToWork - shiftsWorked;
+        }
+    }
+
+    private string currentJob = "";
+    public string CurrentJob
+    {
+        get
+        {
+            return currentJob;
+        }
+    }
+
+    private string[] jobsICanDo;
+    private int shiftsToWork;
+    private int shiftsWorked;
+
+    public bool DoThisJob(string job, int numberOfShifts)
+    {
+        if (!string.IsNullOrEmpty(currentJob))
+        {
+            return false;
+                    for (int i = 0; i < jobsICanDo.Length; i++)
+                        if (jobsICanDo[i] == job)
+                            currentJob = job;
+                            this.shiftsToWork = numberOfShifts;
+                            shiftsWorked = 0;
+                            return true;
+        }
+            return false;      
+    }    
+    
+    public bool DidYouFinish()
+    {
+        if (string.IsNullOrEmpty(currentJob))
+            return false;
+        shiftsWorked++;
+        if (shiftsWorked > shiftsToWork)
+        {
+            shiftsWorked = 0;
+            shiftsToWork = 0;
+            currentJob = "";
+            return true;
+        }
+        else
+            return false;
+    }  
+        
+}
+
+public class Queen
+{
+    public Queen(Worker[] workers)
+    {
+        this.workers = workers;
+    }
+
+    private Worker[] workers;
+    private int shiftNumber = 0;
+
+    public bool AssignWork(string job, int numberOfShifts)
+    {
+        for (int i = 0; i < workers.Length; i++)
+            if (workers[i].DoThisJob(job, numberOfShifts))
+                return true;
+        return false;
+    }
+
+    public string WorkTheNextShift()
+    {
+        shiftNumber++;
+        string report = "Raport zmiany numer " + shiftNumber + "\r\n";
+        for (int i = 0; i < workers.Length; i++)
+        {
+            if (workers[i].DidYouFinish())
+                report += "Robotnica numer " + (i + 1) + " zakończyła swoje zadanie \r\n";
+            if (string.IsNullOrEmpty(workers[i].CurrentJob))
+                report += "Robotnica numer " + (i + 1) + " nie pracuje \r\n";
+            else
+                if (workers[i].ShiftsLeft > 0)
+                report += "Robotnica numer " + (i + 1) + " robi '" + workers[i].CurrentJob + "' jeszcze przez " + workers[i].ShiftsLeft + " zmiany \r\n";
+            else
+                report += "Robotnica numer " + (i + 1) + " zakończy '" + workers[i].CurrentJob + "' po tej zmianie \r\n"
+        }
+        return report;
+    }
+}
--- AlansProject7/Form1.Designer.cs
+++ AlansProject7/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            assignJob = new Button();
+            shifts = new NumericUpDown();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            report = new TextBox();
+            nextShift = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)shifts).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(assignJob);
+            groupBox1.Controls.Add(shifts);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(364, 199);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Przydział prac";
+            // 
+            // assignJob
+            // 
+            assignJob.Location = new Point(15, 130);
+            assignJob.Name = "assignJob";
+            assignJob.Size = new Size(328, 34);
+            assignJob.TabIndex = 3;
+            assignJob.Text = "Przypisz pracę pszczole";
+            assignJob.UseVisualStyleBackColor = true;
+            // 
+            // shifts
+            // 
+            shifts.Location = new Point(214, 66);
+            shifts.Name = "shifts";
+            shifts.Size = new Size(129, 31);
+            shifts.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Zbieranie nektaru" });
+            comboBox1.Location = new Point(15, 64);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // report
+            // 
+            report.Location = new Point(12, 218);
+            report.Multiline = true;
+            report.Name = "report";
+            report.Size = new Size(643, 220);
+            report.TabIndex = 0;
+            // 
+            // nextShift
+            // 
+            nextShift.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            nextShift.Location = new Point(400, 12);
+            nextShift.Name = "nextShift";
+            nextShift.Size = new Size(254, 187);
+            nextShift.TabIndex = 1;
+            nextShift.Text = "Przepracuj następną godzinę";
+            nextShift.UseVisualStyleBackColor = true;
+            nextShift.Click += nextShift_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(666, 450);
+            Controls.Add(nextShift);
+            Controls.Add(report);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "System zarządzania ulem";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)shifts).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Button assignJob;
+        private NumericUpDown shifts;
+        private ComboBox comboBox1;
+        private Label label1;
+        private TextBox report;
+        private Button nextShift;
     }
 }
